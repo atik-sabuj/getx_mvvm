e6082036370e1ee1deee059f16fce4acd69462aa
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-01 00:30:53 +0600

    Created Files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:getx_mvvm/view/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      home: const SplashScreen(),
     );
   }
 }
