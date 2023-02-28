0546e7ce89f76785c80c397d15cb658b50c6b5cf
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-01 00:40:01 +0600

    App Exceptions

---
+++ lib/data/app_exceptions.dart
@@ -0,0 +1,18 @@
+
+class AppExceptions implements Exception {
+
+  final _message;
+  final _prefix;
+
+  AppExceptions([this._message, this._prefix]);
+
+  String toString(){
+    return '$_prefix$_message';
+  }
+}
+
+
+class InternetException extends AppExceptions {
+
+  InternetException([String? message]) : super(message, 'No Internet');
+}
