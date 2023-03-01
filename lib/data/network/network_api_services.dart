ee064375e65be60d358136116c39338073773534
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-01 15:37:02 +0600

    Network Services Api #2

--- lib/data/network/network_api_services.dart
+++ lib/data/network/network_api_services.dart
@@ -1,4 +1,12 @@
 
+import 'package:getx_mvvm/data/network/base_api_services.dart';
+import 'package:http/http.dart' as http;
+
 class NetworkApiServices extends BaseApiServices {
+
+  @override
+  Future<dynamic> getApi(String url)async{
+    final response = await http.get(Uri.parse(url)).timeout(const Duration(seconds: 10));
+  }
 
 }
