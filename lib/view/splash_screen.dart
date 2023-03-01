7fec8aeb5e75a66f4c86a465b8c122630b98966f
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-02 00:31:14 +0600

    Life Time Reusable Components #12

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -4,6 +4,7 @@ import 'package:getx_mvvm/data/app_exceptions.dart';
 import 'package:getx_mvvm/res/assets/image_assets.dart';
 import 'package:getx_mvvm/res/components/general_exceptions_widget.dart';
 import 'package:getx_mvvm/res/components/internet_exceptions_widget.dart';
+import 'package:getx_mvvm/res/components/round_button.dart';
 import 'package:getx_mvvm/utils/utils.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -21,10 +22,12 @@ class _SplashScreenState extends State<SplashScreen> {
         title: Text('Splash Screen'),
         centerTitle: true,
       ),
-      body: GeneralExceptionWidget(
-        onPress: () {
-
-        },),
+      body: Column(
+        children: [
+          RoundButton(onPress: (){}, title: 'Log In',width: double.infinity,loading: true,),
+          RoundButton(onPress: (){}, title: 'Sign Up',width: 200,),
+        ],
+      ),
     );
   }
 }
