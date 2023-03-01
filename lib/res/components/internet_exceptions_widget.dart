f6737b43d4f85d629ddca6ab9d5b066610caf83d
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-01 22:12:02 +0600

    Life Time Reusable Components

---
+++ lib/res/components/internet_exceptions_widget.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class InternetExceptionWidget extends StatefulWidget {
+  const InternetExceptionWidget({Key? key}) : super(key: key);
+
+  @override
+  State<InternetExceptionWidget> createState() => _InternetExceptionWidgetState();
+}
+
+class _InternetExceptionWidgetState extends State<InternetExceptionWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
