201e700dab8cb5d355aafe93687da530aa3db063
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-01 19:58:49 +0600

    Navigation & Routing #3

--- lib/res/routes/routes.dart
+++ lib/res/routes/routes.dart
@@ -1,7 +1,17 @@
 
+import 'package:get/get.dart';
+import 'package:getx_mvvm/res/routes/routes_name.dart';
+import 'package:getx_mvvm/view/splash_screen.dart';
+
+
 class AppRoutes {
 
   static appRoute () => [
-
+    GetPage(name: RouteName.splashScreen,
+        page: () => SplashScreen(),
+      transitionDuration: Duration(milliseconds: 250),
+      transition: Transition.leftToRightWithFade,
+    ),
   ];
+
 }
