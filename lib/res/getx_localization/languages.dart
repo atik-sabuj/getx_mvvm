5efaa91c095fe7231c03f8a8d5845658877a77f5
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-01 21:05:29 +0600

    App Strings & GetX Localization #2

--- lib/res/getx_localization/languages.dart
+++ lib/res/getx_localization/languages.dart
@@ -11,5 +11,17 @@ class Languages extends Translations {
       'email_hint' : 'Enter email'
     },
 
+    'ban_BD' : {
+      'email_hint' : 'ইমেইল প্রদান করুন'
+    },
+
+    'ur_PK' : {
+      'email_hint' : 'ای میل درج کریں۔'
+    },
+
+    'hin_IND' : {
+      'email_hint' : 'ईमेल दर्ज करें'
+    },
+
   };
 }
