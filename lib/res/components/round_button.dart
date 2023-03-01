8a7ed3d41e7230bb7bc3291e77f87f2417e176ad
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-02 00:25:18 +0600

    Life Time Reusable Components #11

--- lib/res/components/round_button.dart
+++ lib/res/components/round_button.dart
@@ -3,12 +3,18 @@ import 'package:getx_mvvm/res/colors/app_color.dart';
 
 
 class RoundButton extends StatelessWidget {
+
   const RoundButton({
     Key? key,
     this.buttonColor = AppColor.primaryButtonColor,
     this.textColor = AppColor.primaryTextColor,
-
+    required this.title,
+    required this.onPress,
+    this.width = 60,
+    this.height = 50,
+    this.loading = false,
   }) : super(key: key);
+
 
   final bool loading;
   final String title;
@@ -18,6 +24,18 @@ class RoundButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        color: buttonColor,
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: loading ?
+      Center(child: CircularProgressIndicator()) :
+      Center(
+        child: Text(title, style: Theme.of(context).textTheme.titleMedium!.copyWith(color: Colors.white),),
+      ),
+    );
   }
 }
