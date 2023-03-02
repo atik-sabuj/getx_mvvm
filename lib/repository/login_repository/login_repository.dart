78f0f71a624e6a69fcc9009a576266afeb51d5e4
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-02 16:59:35 +0600

    Login Repository with API #2

--- lib/repository/login_repository/login_repository.dart
+++ lib/repository/login_repository/login_repository.dart
@@ -1,8 +1,15 @@
 
 
+import 'dart:ui';
+
 import 'package:getx_mvvm/data/network/network_api_services.dart';
 
 class LoginRepository {
 
   final _apiService = NetworkApiServices();
+
+  Future<dynamic> loginApi(var data) async{
+    dynamic response = _apiService.postApi(data, url);
+    return response;
+  }
 }
