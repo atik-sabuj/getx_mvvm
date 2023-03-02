4ae7d44cdf2099730e767dbd9b87e3e4a4b45d07
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-02 17:16:52 +0600

    Login View Model to hit API

--- lib/view_models/controller/login_view_model.dart
+++ lib/view_models/controller/login_view_model.dart
@@ -2,8 +2,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_mvvm/repository/login_repository/login_repository.dart';
+import 'package:getx_mvvm/utils/utils.dart';
 
 class LoginViewModel extends GetxController{
+
+  final _api = LoginRepository();
 
   final emailController = TextEditingController().obs;
   final passwordController = TextEditingController().obs;
@@ -12,8 +16,17 @@ class LoginViewModel extends GetxController{
   final passwordFocusNode = FocusNode().obs;
 
 
+
   void loginApi(){
+    Map data = {
+      'email' : emailController.value.text,
+      'password' : passwordController.value.text,
+    };
 
-
+    _api.loginApi(data).then((value){
+      Utils.snakBar('Log In', 'Login Successfully');
+    }).onError((error, stackTrace){
+      Utils.snakBar('Error', error.toString());
+    });
   }
 }
