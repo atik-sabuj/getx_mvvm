98c6a7d994d68bce13927a228f1c913f57f7cdb6
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-03 15:48:37 +0600

    Share Preference To Manage Session

--- lib/view_models/controller/user_preference/user_preference_view_model.dart
+++ lib/view_models/controller/user_preference/user_preference_view_model.dart
@@ -1,11 +1,23 @@
 
 import 'package:get/get.dart';
-import 'package:getx_mvvm/models/login/login_response_model.dart';
+import 'package:getx_mvvm/models/login/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 class UserPreference extends GetxController {
 
-  Future<bool> saveUser(LoginResponseModel responseModel)async{
-
+  Future<bool> saveUser(UserModel responseModel)async{
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    sp.setString('token', responseModel.token.toString());
     return true;
+  }
+
+
+  Future<UserModel> getUser()async{
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    String? token = sp.getString('token');
+    return UserModel(
+      token: token
+    );
   }
 }
