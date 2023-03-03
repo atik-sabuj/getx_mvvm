166e8b4ac2b12260e59c6fe643ba10d62ec78ab2
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-03 15:38:26 +0600

    User Preference

---
+++ lib/view_models/controller/user_preference/user_preference_view_model.dart
@@ -0,0 +1,11 @@
+
+import 'package:get/get.dart';
+import 'package:getx_mvvm/models/login/login_response_model.dart';
+
+class UserPreference extends GetxController {
+
+  Future<bool> saveUser(LoginResponseModel responseModel)async{
+
+    return true;
+  }
+}
