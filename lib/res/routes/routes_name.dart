48358d03705c909690d90d170e65f12925d8002d
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-08 14:00:52 +0600

    Code Refactoring #3

--- lib/res/routes/routes_name.dart
+++ lib/res/routes/routes_name.dart
@@ -1,10 +1,11 @@
 
 class RouteName {
 
-  static const String homeView = '/home_View';
+  static const String loginView = '/';
 
   static const String splashScreen = '/splash_Screen';
 
-  static const String loginView = '/';
+  static const String homeView = '/home_View';
+
 
 }
