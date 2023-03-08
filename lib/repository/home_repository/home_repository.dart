7226b1359b74c83cac071c9bf3b6691a7bc6c1a8
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-08 10:57:54 +0600

    Created Home Repository

---
+++ lib/repository/home_repository/home_repository.dart
@@ -0,0 +1,16 @@
+
+import 'dart:ui';
+
+import 'package:getx_mvvm/data/network/network_api_services.dart';
+import 'package:getx_mvvm/models/home/user_list_model.dart';
+import 'package:getx_mvvm/res/app_url/app_url.dart';
+
+class HomeRepository {
+
+  final _apiService = NetworkApiServices();
+
+  Future<UserListModel> userListApi(var data) async{
+    dynamic response = await _apiService.getApi(AppUrl.userListApi);
+    return UserListModel.fromJson(response);
+  }
+}
--- lib/res/app_url/app_url.dart
+++ lib/res/app_url/app_url.dart
@@ -3,6 +3,7 @@
 class AppUrl {
 
   static const String baseUrl = 'https://reqres.in';
-
   static const String loginApi = '$baseUrl/api/login';
+
+  static const String userListApi = 'https://webhook.site/898a88f9-7b3c-4ff9-be73-683156ef1f2e';
 }
